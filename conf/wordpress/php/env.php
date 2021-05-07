b7e6f6cefb4b2c1bb8d9d719e8301cc7b3373e47
Sean O'Shaughnessy <seanosh@gmail.com>
2021-05-06 21:06:13 -0400

    we're spinning up mailcatcher but not using it, fix that

--- conf/wordpress/php/env.php
+++ conf/wordpress/php/env.php
@@ -11,17 +11,16 @@ define( 'AUTOMATIC_UPDATER_DISABLED', true );
 define( 'WPCOM_IS_VIP_ENV', false );
 define( 'WP_ENVIRONMENT_TYPE', 'local' );
 define( 'VIP_GO_APP_ENVIRONMENT', 'local' );
-define( 'VIP_BLOCK_WP_MAIL', true );
 define( 'FILES_CLIENT_SITE_ID', 1 );
 
 define( 'VIP_VAULTPRESS_SKIP_LOAD', true );
 define( 'VIP_JETPACK_CONNECTION_PILOT_SHOULD_RUN', false );
 
-/*
+
 $all_smtp_servers = array (
 	0 => 'mailcatcher:1025',
 );
-*/
+
 
 $memcached_servers = array (
 	'default' =>
