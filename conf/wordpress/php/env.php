4551bde676923e11313e22f7cc43c07faa4fd966
Sean O'Shaughnessy <seanosh@gmail.com>
2022-08-25 07:44:11 -0400

    add 'FILES_ACCESS_TOKEN' so that a8c-files.php logic works for better parity

--- conf/wordpress/php/env.php
+++ conf/wordpress/php/env.php
@@ -12,6 +12,7 @@ define( 'WPCOM_IS_VIP_ENV', false );
 define( 'WP_ENVIRONMENT_TYPE', 'local' );
 define( 'VIP_GO_APP_ENVIRONMENT', 'local' );
 define( 'FILES_CLIENT_SITE_ID', 1 );
+define( 'FILES_ACCESS_TOKEN', 'abc123' );
 
 define( 'VIP_VAULTPRESS_SKIP_LOAD', true );
 define( 'VIP_JETPACK_CONNECTION_PILOT_SHOULD_RUN', false );
