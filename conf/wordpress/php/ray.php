205f84e028c7431f947829fde59b165d233bef34
Sean O'Shaughnessy <seanosh@gmail.com>
2022-09-16 10:35:40 -0400

    enable ray support (https://myray.app/)

---
+++ conf/wordpress/php/ray.php
@@ -0,0 +1,22 @@
+<?php
+return [
+	/*
+	 *  The host used to communicate with the Ray app.
+	 */
+	'host' => 'host.docker.internal',
+
+	/*
+	 *  The port number used to communicate with the Ray app. 
+	 */
+	'port' => 23517,
+	
+	/*
+	 *  Absolute base path for your sites or projects in Homestead, Vagrant, Docker, or another remote development server.
+	 */
+	'remote_path' => '/var/www/html',
+	
+	/*
+	 *  Absolute base path for your sites or projects on your local computer where your IDE or code editor is running on. 
+	 */
+	//'local_path' => '',
+];
